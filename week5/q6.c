95c5b14ae2dd14ac33388e6ad1105f14aa72935a
Evan Williams <evanawilliams@live.com>
2022-06-28 13:19:41 +1000

    week5

---
+++ week5/q6.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+typedef uint32_t Word;
+
+Word reverseBits(Word w) {
+    Word res = 0;
+    for (Word i = 0; i < 32; i++) {
+        // use an and to get the ith bit
+        Word current_bit = (w & (1u << i)) >> i;
+        Word result_index = 32 - i;
+        // Set the result index to the current bit
+        res = res | (current_bit << result_index);
+    }
+
+    return res;
+}
